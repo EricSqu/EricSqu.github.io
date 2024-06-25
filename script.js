7bb61de57316ea88eeda9eb44374291609fb9dd9
EricSqu <140271387+EricSqu@users.noreply.github.com>
2024-06-25 17:59:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -30,4 +30,17 @@ function type() {
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Document loaded'); // Add this line
     setTimeout(type, 1000);
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
 });
