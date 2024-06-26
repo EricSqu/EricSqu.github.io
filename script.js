d7e9f08ace4503bd21bd506beee62f95b3340320
EricSqu <140271387+EricSqu@users.noreply.github.com>
2024-06-26 12:44:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const dynamicJob = document.getElementById('dynamic-job');
 
-const titles = ["and I am a Software Engineer", "and I am a Computer Science Student", "and I am a Creator", "and I am a Web Developer"];
+const jobTitles = ["Software Engineer", "Computer Science Student", "Creator", "Web Developer"];
 let titleIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
 
 function type() {
-    const currentTitle = titles[titleIndex];
+    const currentTitle = jobTitles[titleIndex];
     if (isDeleting) {
         dynamicJob.textContent = currentTitle.substring(0, charIndex - 1);
         charIndex--;
         if (charIndex === 0) {
             isDeleting = false;
-            titleIndex = (titleIndex + 1) % titles.length;
+            titleIndex = (titleIndex + 1) % jobTitles.length;
         }
     } else {
         dynamicJob.textContent = currentTitle.substring(0, charIndex + 1);
